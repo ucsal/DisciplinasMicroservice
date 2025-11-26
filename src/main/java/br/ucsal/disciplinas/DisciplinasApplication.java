e7631a62754688ad83323a01803412131361d518
TGBastos <dm.thiagogb@hotmail.com>
2025-11-25 23:16:27 -0300

    Add Eureka

--- src/main/java/br/ucsal/disciplinas/DisciplinasApplication.java
+++ src/main/java/br/ucsal/disciplinas/DisciplinasApplication.java
@@ -3,6 +3,9 @@ package br.ucsal.disciplinas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class DisciplinasApplication {
 
